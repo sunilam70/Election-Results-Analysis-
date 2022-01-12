b2e8d852f4911a0c07203df5388f6857b218fd99
sunilam70 <96486057+sunilam70@users.noreply.github.com>
2022-01-11 20:18:21 -0500

    Add files via upload
    Grocery homework file

---
+++ grocery_list.py
@@ -0,0 +1,16 @@
+# Create a Python list to store your grocery list
+grocery_list=["Milk","Bread","Eggs","Peanut Butter","Jelly"]
+# Print the grocery list
+print(grocery_list)
+
+# Change "Peanut Butter" to "Almond Butter" and print out the updated list
+grocery_list[3]="Almond Butter"
+print(grocery_list)
+
+# Remove "Jelly" from grocery list and print out the updated list
+grocery_list.remove("Jelly")
+print(grocery_list)
+
+# Add "Coffee" to grocery list and print the updated list
+grocery_list.append("coffee")
+print(grocery_list)
